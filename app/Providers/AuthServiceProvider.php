f6e7d130626d15ea2f898902d70d8d76a5685edf
Benny Kaiser <bkaiser@embl.fr>
2025-02-12 11:49:59 +0100

    Client/Portal -> PortalClient

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,10 +12,17 @@ class AuthServiceProvider extends IlluminateAuthServiceProvider
 
     protected $policies = [];
 
+    public function register()
+    {
+        $this->app->singleton(PortalClient::class, function ($app) {
+            return new PortalClient();
+        });
+        Auth::provider('portal', function ($app) {
+            return new UserProvider($app->make(PortalClient::class));
+        });
+    }
+
     public function boot()
     {
-        Auth::provider('portal', function ($app, array $config) {
-            return new UserProvider(new PortalClient());
-        });
     }
 }
