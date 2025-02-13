8867b7c9183020b25fd9da2b511e60899b2cd67d
Benny Kaiser <bkaiser@embl.fr>
2025-02-13 16:03:08 +0100

    added middleware groups

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Portal\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Support\Facades\Auth;
--- app/Http/Middleware/Janitor.php
+++ app/Http/Middleware/Janitor.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Portal\Http\Middleware;
 
 use Portal\Models\User;
 use Portal\Models\Role;
